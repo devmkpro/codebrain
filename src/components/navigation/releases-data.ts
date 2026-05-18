5a4de67da1d964d60d23ef6e1c4af3b3285392a8
devmkpro <devmkbr@proton.me>
2026-05-18 12:42:30 -0300

    fix: update release date for version 1.3.3

--- src/components/navigation/releases-data.ts
+++ src/components/navigation/releases-data.ts
@@ -15,7 +15,7 @@ export interface Release {
 export const RELEASES: Release[] = [
   {
     "version": "1.3.3",
-    "date": "em desenvolvimento",
+    "date": "maio 2026",
     "highlights": [
       {
         "kind": "feat",
