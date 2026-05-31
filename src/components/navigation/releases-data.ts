0b12acbcac77ad55417c17175c0983a03dffe5dc
devmkpro <devmkbr@proton.me>
2026-05-31 00:39:27 -0300

    chore: regenerate releases-data.ts for v1.8.12

--- src/components/navigation/releases-data.ts
+++ src/components/navigation/releases-data.ts
@@ -14,7 +14,7 @@ export interface Release {
 
 export const RELEASES: Release[] = [
   {
-    "version": "1.8.11",
+    "version": "1.8.12",
     "date": "em desenvolvimento",
     "highlights": [
       {
